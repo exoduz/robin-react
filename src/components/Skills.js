a21961d748856b7b824925fec846c28837393a37
Robin Roy Julius <exo_duz@yahoo.com>
2016-05-31 11:46:56 +0800

    Updated skill levels

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -9,32 +9,32 @@ class Skills extends React.Component {
 			programming: {
 				programming1: { name: 'HTML5', class_name: 'html', level: 99 },
 				programming2: { name: 'CSS3', class_name: 'css', level: 99 },
-				programming3: { name: 'PHP', class_name: 'php', level: 85 },
-				programming4: { name: 'Javascript', class_name: 'js', level: 70 },
+				programming3: { name: 'PHP', class_name: 'php', level: 90 },
+				programming4: { name: 'Javascript', class_name: 'js', level: 85 },
 				programming5: { name: 'C#', class_name: 'csharp', level: 60 }
 			},
 			database: {
-				database1: { name: 'MySQL', class_name: 'mysql', level: 75 },
+				database1: { name: 'MySQL', class_name: 'mysql', level: 85 },
 				database2: { name: 'MS SQL Server', class_name: 'sqlserver', level: 60 }
 			},
 			frameworks: {
-				framework1: { name: 'CakePHP', class_name: 'cakephp', level: 85 },
-				framework2: { name: 'WordPress', class_name: 'wordpress', level: 80 },
+				framework1: { name: 'CakePHP', class_name: 'cakephp', level: 90 },
+				framework2: { name: 'WordPress', class_name: 'wordpress', level: 85 },
 				framework3: { name: 'React.js', class_name: 'react-js', level: 75 },
 				framework4: { name: '.NET', class_name: 'dotnet', level: 60 }
 			},
 			webserver: {
-				webserver1: { name: 'Apache', class_name: 'apache', level: 75 },
-				webserver2: { name: 'Nginx', class_name: 'nginx', level: 75 },
+				webserver1: { name: 'Apache', class_name: 'apache', level: 85 },
+				webserver2: { name: 'Nginx', class_name: 'nginx', level: 85 },
 				webserver3: { name: 'IIS', class_name: 'iis', level: 75 }
 			},
 			os: {
 				os1: { name: 'OSX', class_name: 'osx', level: 95 },
 				os2: { name: 'Windows', class_name: 'windows', level: 90 },
-				os3: { name: 'Linux', class_name: 'linux', level: 70 }
+				os3: { name: 'Linux', class_name: 'linux', level: 75 }
 			},
 			software: {
-				software1: { name: 'Microsoft Office', class_name: 'office', level: 95 },
+				software1: { name: 'Microsoft Office', class_name: 'office', level: 99 },
 				software2: { name: 'Sublime', class_name: 'sublime', level: 90 },
 				software3: { name: 'Adobe Suite', class_name: 'adobe', level: 70 },
 				software4: { name: 'Visual Studio Suite', class_name: 'visualstudio', level: 70 },
