e29b56f63870899859a710f349d11d461efbfada
Robin Roy Julius <exo_duz@yahoo.com>
2016-06-02 13:53:57 +0800

    Refactored validation to return when first error found in ContactForm.js. There's no need to loop through everything else once an error is found.

--- src/components/ContactForm/InputError.js
+++ src/components/ContactForm/InputError.js
@@ -2,11 +2,16 @@ import React from 'react';
 
 class InputError extends React.Component {
 	render() {
-		return (
-			<label id="name-error" className="error" for="name">This field is required.</label>
-		)
+		return <label id="name-error" className="error" for="name">{ this.props.errorMessage }</label>;
 	}
 }
 
+InputError.defaultProps = {
+  errorMessage: 'This input is invalid.'
+};
+
+InputError.propTypes = {
+	errorMessage: React.PropTypes.string.isRequired
+}
 
 export default InputError;
