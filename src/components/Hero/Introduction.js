977763a80fd7ba1fc74fafa1c3f2095a55faef5b
Robin Roy Julius <exo_duz@yahoo.com>
2016-06-09 17:14:27 +0800

    Added robots.txt Fixed broken links

--- src/components/Hero/Introduction.js
+++ src/components/Hero/Introduction.js
@@ -10,7 +10,7 @@ class Introduction extends React.Component {
 	          Nice to meet you.<br />よろしくお願いします。<br />Senang bertemu denganmu.<br />幸会，幸会。
 	        </h2>
 	        <p>I'm a website generalist, who loves everything about websites. There is a beauty in the simplicity of design. I strive to make my websites simple, yet intuitive to all users.</p>
-	        <p>If you want to know a little more about me, you can view my <a href="https://linkedin.com/in/robinjulius" target="_blank"><i className="fa fa-linkedin"></i> Linkedin profile</a> or download my <a href="files/RobinRESUME.pdf" target="_blank"><i className="fa fa-file-text-o"></i> CV.</a> I also <a href="http://blog.robinjulius.com" target="_blank"><i className="fa fa-rss"></i> blog</a> occasionally.</p>
+	        <p>If you want to know a little more about me, you can view my <a href="https://linkedin.com/in/robinjulius" target="_blank"><i className="fa fa-linkedin"></i> Linkedin profile</a> or download my <a href="files/RobinRESUME.pdf" target="_blank"><i className="fa fa-file-text-o"></i> CV.</a> I also <a href="blog/" target="_blank"><i className="fa fa-rss"></i> blog</a> occasionally.</p>
 	        <p>I would love to hear from you. Say hi through <a href="#contact" className="smooth-link"><i className="fa fa-envelope-o"></i> the contact form</a>.</p>
 	        
 	        <div id="scroll-for-more-info">
