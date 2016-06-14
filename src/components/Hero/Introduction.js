562bff97453517dc0959992177b9f384acbd39ec
Robin Roy Julius <exo_duz@yahoo.com>
2016-06-14 17:05:32 +0800

    Updated text and added link to source code on Github in the introduction

--- src/components/Hero/Introduction.js
+++ src/components/Hero/Introduction.js
@@ -10,7 +10,8 @@ class Introduction extends React.Component {
 	          Nice to meet you.<br />よろしくお願いします。<br />Senang bertemu denganmu.<br />幸会，幸会。
 	        </h2>
 	        <p>I'm a website generalist, who loves everything about websites. There is a beauty in the simplicity of design. I strive to make my websites simple, yet intuitive to all users.</p>
-	        <p>If you want to know a little more about me, you can view my <a href="https://linkedin.com/in/robinjulius" target="_blank"><i className="fa fa-linkedin"></i> Linkedin profile</a> or download my <a href="assets/files/RobinRESUME.pdf" target="_blank"><i className="fa fa-file-text-o"></i> CV.</a> I also <a href="blog/"><i className="fa fa-rss"></i> blog</a> occasionally.</p>
+	        <p>Here's some ways to get to know me better, my <a href="https://linkedin.com/in/robinjulius" target="_blank"><i className="fa fa-linkedin"></i> Linkedin profile</a> or download my <a href="assets/files/RobinRESUME.pdf" target="_blank"><i className="fa fa-file-text-o"></i> CV.</a> I also <a href="blog/"><i className="fa fa-rss"></i> blog</a> occasionally.</p>
+	        <p>This website was recently rewritten from <a href="http://php.net/" target="_blank">PHP</a> to <a href="https://facebook.github.io/react/" target="_blank">React</a> and the <a href="https://github.com/exoduz/robin-react" target="_blank"><i className="fa fa-code"></i> source code</a> is available on <a href="https://github.com/exoduz">Github <i className="fa fa-github"></i></a>.</p>
 	        <p>I would love to hear from you. Say hi through <a href="#contact" className="smooth-link"><i className="fa fa-envelope-o"></i> the contact form</a>.</p>
 	        
 	        <div id="scroll-for-more-info">
